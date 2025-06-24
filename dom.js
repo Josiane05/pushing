f06ca903c60b96c0fc88d117371bfeb17885dbd1
Josiane05 <josianemushimiyimana05@gmail.com>
2025-06-24 21:08:09 +0100

    changing text content of element

--- dom.js
+++ dom.js
@@ -2,3 +2,4 @@ const element = document.getElementById(id);
 <p id="first">Hi, There!</p>
 <p>JavaScript is fun.</p>
 const elem = document.getElementById("first");
+document.getElementId('footer').innerHTML = 'copyright';
