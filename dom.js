30ab0207e73215baaba04de207aaa84656c2de86
Josiane05 <josianemushimiyimana05@gmail.com>
2025-06-26 21:13:10 +0100

    get previous sibling in js

--- dom.js
+++ dom.js
@@ -2,3 +2,7 @@ let current = document.querySelector('.current');
 let nextSibling = current.nextElementSibling;
 
 console.log(nextSibling);
+let curre = document.querySelector('.current');
+let prevSiblings = current.previousElementSibling;
+
+console.log(prevSiblings);
