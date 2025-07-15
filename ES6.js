6ed77736b025d9c6f5408547ba1683ae92a369a8
Josiane05 <josianemushimiyimana05@gmail.com>
2025-07-15 13:16:34 +0100

    object destructing

--- ES6.js
+++ ES6.js
@@ -1,3 +1,11 @@
 const colors = ['green','yellow','blue'];
 const items = colors.map(color =>`<li>${color}</li>` );
 console.log(items);
+
+const address = {
+    street: '',
+    city: '',
+    country: '',
+}
+
+const {street:st, city, country} = address;
