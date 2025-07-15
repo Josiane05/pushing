cb1f0aeebf77a07f054b6d2fd68d059ac1097799
Josiane05 <josianemushimiyimana05@gmail.com>
2025-07-15 13:49:33 +0100

    CLASS

--- ES6.js
+++ ES6.js
@@ -2,3 +2,14 @@ const first = { name: 'JOSIANE'};
 const second = { job: 'IT'};
 const clone = {...first};
 console.log(clone);
+
+class person {
+    constructor(name) {
+        this.name = name;
+    }
+    walk() {
+        console.log("walk")
+    }
+}
+
+const person = new person ('JOSIANE');
