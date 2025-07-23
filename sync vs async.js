46332a19cc0103d88abb18d04df7672499d1f75b
Josiane05 <josianemushimiyimana05@gmail.com>
2025-07-23 21:03:25 +0100

    promises

--- sync vs async.js
+++ sync vs async.js
@@ -1,25 +1,5 @@
-// a synchronous => not accuring at the same time
+const promise = new promise (function(resolve, reject){
+    resolve('I am resolved promise!');
+});
 
-// fetch data from server
-// want to execute something with a delay
-// want to execute something after an event
-
-//  1.browser APIs / web APIs
-// setTimeout, mouse over, click
-
-// 2.promises => unique javascript object that allow you to make asynchronous operations
-
-function f1() {
-    console.log('f1');
-}
-
-function f2() {
-    console.log('f2');
-}
-function main() {
-    console.log('main')
-    setTimeout(f1,f2,2000);
-}
-
-main();
-
+promise.then(result => console.log(result));
