2f0f290128c9f9e52b670d80bd62bb8d70704d29
Josiane05 <josianemushimiyimana05@gmail.com>
2025-07-23 21:22:12 +0100

    queue

--- sync vs async.js
+++ sync vs async.js
@@ -1,5 +1,22 @@
-const promise = new promise (function(resolve, reject){
-    resolve('I am resolved promise!');
-});
+function f1() {
+    console.log('f1');
+}
 
-promise.then(result => console.log(result));
+function f2() {
+    console.log('f2');
+}
+
+function main() {
+    console.log('main');
+    setTimeout(f1, 0);
+
+    new Promise(function (resolve, reject) {
+    resolve('I am promise');
+}).then(resolve => console.log('resolve'));
+
+f2();
+}
+
+main();
+
+
