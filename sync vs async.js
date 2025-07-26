dbf1886b2ed6547b02cfed9a581441e69a65c33e
Josiane05 <josianemushimiyimana05@gmail.com>
2025-07-26 15:39:41 +0100

    promise and handle

--- sync vs async.js
+++ sync vs async.js
@@ -1,26 +1,12 @@
-console.log('introduction to Promises!');
-
+// .then();
+// .catch();
+// .finally();
 let promise = new promise(function (resolve, reject){
     //got the water
     let value = 'water'
     resolve ('value');
 }); 
 
-
-let promiseError = new promise(function (resolve, reject){
-    //opps, jack fell down
-    reject(new Error("Disasters"));
+promise.then(function(result) {
+    console.log(result);
 });
-
-//promise state(internal)
-//- pending: when execution start
-//-fullfilled: when resolves successfully
-//-rejcted: when promise rejectes
-
-//if promise fullfilled/rejected => settled
-
-//results
-
-//-Undefined: initial phase when state is pending
-//-resolve: when promise resolves successfully
-//-Error: when promise is rejects
