d6c47da08f707e665036f734a9bf8970146aa4a4
Josiane05 <josianemushimiyimana05@gmail.com>
2025-07-26 21:01:59 +0100

    callback

--- sync vs async.js
+++ sync vs async.js
@@ -1,16 +1,5 @@
-// .then(); => it return the promise from it,hansle reject scenario
-// .catch(); => it handle reject scenario
-// .finally();
-let promise = new promise(function (resolve, reject){
-    //got the water
-    let value = 'water'
-    resolve ('value');
-}); 
+console.log('#3. callback');
 
-const grandParentCooking = () => { 
-    promise.then(function(result) {
-    console.log(`cooking with $ {result}`);
-});
+function foo() {
+    console.log('foo');
 }
-
-grandParentCooking();
