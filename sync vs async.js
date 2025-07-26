0b525b6d52c3e15fa4b888605ee9ffdf23a9a154
Josiane05 <josianemushimiyimana05@gmail.com>
2025-07-26 15:28:06 +0100

    promise State promise result expected

--- sync vs async.js
+++ sync vs async.js
@@ -11,3 +11,16 @@ let promiseError = new promise(function (resolve, reject){
     //opps, jack fell down
     reject(new Error("Disasters"));
 });
+
+//promise state(internal)
+//- pending: when execution start
+//-fullfilled: when resolves successfully
+//-rejcted: when promise rejectes
+
+//if promise fullfilled/rejected => settled
+
+//results
+
+//-Undefined: initial phase when state is pending
+//-resolve: when promise resolves successfully
+//-Error: when promise is rejects
