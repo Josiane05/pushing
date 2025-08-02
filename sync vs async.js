01e8b9bf9768d0605fdf6f52967e6ffaf6bc95e3
Josiane05 <josianemushimiyimana05@gmail.com>
2025-08-02 14:30:46 +0100

    promose.then() callback

--- sync vs async.js
+++ sync vs async.js
@@ -8,3 +8,11 @@ console.log('promise chain');
 const promise = new promise(function (resolve, reject) {
     console.log('resolving fake promise');
 });
+
+//handle it using the .then() method
+// .then()method take another callback function with the resolved value
+
+promise.then(function(value) {
+    console.log('value');
+})
+
